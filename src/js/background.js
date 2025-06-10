f798c0d3ee6e2d01e7e211e98589804d6e0a252f
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-06-10 18:42:37 +0900

    background.jsをリファクタリング

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,23 @@
 (() => {
-  chrome.runtime.onMessage.addListener((request, _, callback) => {
+  "use strict";
+
+  const handlePostCommentRequest = (callback) => {
+    chrome.runtime.getPlatformInfo((info) => {
+      if (chrome.runtime.lastError) {
+        console.error("Failed to get platform info:", chrome.runtime.lastError);
+        callback({ os: "unknown" });
+        return;
+      }
+      callback(info);
+    });
+  };
+
+  const messageHandler = (request, _, callback) => {
     if (request.action === "post_comment") {
-      chrome.runtime.getPlatformInfo((info) => {
-        callback(info);
-      });
+      handlePostCommentRequest(callback);
     }
     return true;
-  });
+  };
+
+  chrome.runtime.onMessage.addListener(messageHandler);
 })();
